4ae14f5f4bbbe5546d2695b80fb5bf0a9348d660
Danny Yoo <dyoo@cs.wpi.edu>
2011-06-26 15:42:39 -0400

    trying to get initialization in place.

---
+++ private/sdl.rkt
@@ -0,0 +1,29 @@
+#lang racket/base
+
+(require ffi/unsafe)
+         
+
+(define libsdl (ffi-lib "libSDL" #f))
+
+
+(define SDL_INIT_TIMER          #x00000001)
+(define SDL_INIT_AUDIO          #x00000010)
+(define SDL_INIT_VIDEO          #x00000020)
+(define SDL_INIT_CDROM          #x00000100)
+(define SDL_INIT_JOYSTICK       #x00000200)
+(define SDL_INIT_NOPARACHUTE    #x00100000)
+(define SDL_INIT_EVENTTHREAD    #x01000000)
+(define SDL_INIT_EVERYTHING     #x0000FFFF)
+
+
+
+
+
+
+;; We want to expose libsdl for use with Racket programs.
+
+
+
+;; SDL_Init
+
+;; SDL_Quit
